6245fc059118881383068c532a047845f68a8567
Gataze <61094615+Gataze@users.noreply.github.com>
2021-10-14 11:09:31 +0200

    First portfolio website v1.2

--- src/menu/BurgerBtn.js
+++ src/menu/BurgerBtn.js
@@ -1,21 +1,14 @@
 import { useState } from 'react';
 import './../styles/burger-btn-styles/burger-btn.css'
 
-
-
 const BurgerBtn = ({activeStyle, handleShow}) => {
 
-    
     const [active, setActive] = useState(false)
-    
-
+  
     const handleClick = () => {
         setActive(!active)
         handleShow(activeStyle)
     }
-
-    
-
 
     return ( 
         <button onClick={() => handleClick(active)} className={!active? "hamburger" : "hamburger hamburger--active"} aria-label="Menu"> 
@@ -24,6 +17,6 @@ const BurgerBtn = ({activeStyle, handleShow}) => {
                 </span> 
         </button>
      );
-}
+};
  
 export default BurgerBtn;
