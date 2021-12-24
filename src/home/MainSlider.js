29690ec3e2eb81fd856555a3669848b44b43efdb
Marcin <b.gataze@gmail.com>
2021-12-24 17:51:31 +0100

    v1.2.3. + BEM convention for main-slider styles added.

--- src/home/MainSlider.js
+++ src/home/MainSlider.js
@@ -12,60 +12,69 @@ const MainSlider = () => {
 
 
     return ( 
-        <div className="hero">
-            <div className="row">     
-                <div className="col">
-                {isPending && <div>Loading</div>}
-                {error && <div>{error}</div>}
-                {slides && 
-                     <Carousel infiniteLoop={true} showThumbs={false} autoFocus={true} autoPlay={true} interval={5000} transitionTime={1000}>
-                     {slides.map(slide => (
-                         <div className="main-slider" key={slide.id} style={{backgroundImage: `url(${slide.url})`}}>
-                         
-                         </div>
-                     ))}                     
-                     </Carousel>
-       
-                }    
-                <div className="hero-msg">
-                    <h2>NEW MODEL</h2>
-                    <p>Manticora</p>
-                    <Link to={`/products/bikesPreview/1`}>Check It Now</Link>
-                    <FaArrowLeft className='arrow'/>
-                    </div>
+        <section className="Hero">
+            <div className="Hero__container">     
+                <div className="Hero__slide">
+                    {isPending && <div>Loading</div>}
+                    {error && <div>{error}</div>}
+                    
+                    {slides && 
+                        <Carousel className="Hero__carousel" infiniteLoop={true} showThumbs={false} autoFocus={true} autoPlay={true} interval={5000} transitionTime={1000}>
+                            {slides?.map(slide => (
+                                <div className="Hero__carouselItem" key={slide.id} style={{backgroundImage: `url(${slide.url})`}}></div>
+                            ))}                     
+                        </Carousel>}
+                      
+
+                        <div className="Hero__sliderText">
+                            <h2>NEW MODEL</h2>
+                            <p>Manticora</p>
+                            <Link className="Hero__sliderLink" to={`/products/bikesPreview/1`}>Check It Now</Link>
+                            <FaArrowLeft className='Hero__arrow'/>
+                        </div>
+
                 </div>  
-            </div>
-            <div className="row-2">
-                <div className="col">
-                    <div className="square">
-                        <FaUserShield />
+        
+                <ul className="Hero__list">
+                                
+                    <li className="Hero__listItem">
+                        <div className="Hero__square">
+                            <FaUserShield />
+                        </div>
+                        <h2>Safety first</h2>
+                        <p>SAFETY BY DEISGN</p>
+                    </li>
+                    
+                    <li className="Hero__listItem">
+                        <div className="Hero__square">
+                            <FaPhoneAlt />
+                        </div>
+                        <h2>Support 24/7h</h2>
+                        <p>We support 24h a day</p>
+                    </li>
+
+                    <li className="Hero__listItem">
+
+                        <div className="Hero__square">
+                            <FaToolbox />
+                        </div>
+                        <h2>Expert service</h2>
+                        <p>Profesionals who know</p>
+                    </li>
+
+                    <div className="Hero__listItem">
+                        <div className="Hero__square">
+                            <FaTools />
+                        </div>
+                        <h2>Repair</h2>
+                        <p>Expert Mechanics</p>
                     </div>
-                    <h2>Safety first</h2>
-                    <p>SAFETY BY DEISGN</p>
-                </div>
-                <div className="col">
-                    <div className="square">
-                        <FaPhoneAlt />
-                    </div>
-                    <h2>Support 24/7h</h2>
-                    <p>We support 24h a day</p>
-                </div>
-                <div className="col">
-                    <div className="square">
-                        <FaToolbox />
-                    </div>
-                    <h2>Expert service</h2>
-                    <p>Profesionals who know</p>
-                </div>
-                <div className="col">
-                    <div className="square">
-                        <FaTools />
-                    </div>
-                    <h2>Repair</h2>
-                    <p>Expert Mechanics</p>
-                </div>
-            </div>
-        </div>
+
+                </ul>
+
+            </div>        
+        </section>
+        
      );
 }
  
